ec15142ca43c344a7e908c0f5d24c30ab8d2d5ad
thenormalpassword <122403709+thenormalpassword@users.noreply.github.com>
2023-02-16 14:55:39 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,11 @@
 def league_points(wins, draws, losses):
-    ...
-
-
-
-
-    
+ 
+ wins = 3 * wins 
+ draws = 1 * draws
+ losses = 0 * losses
+ 
+ total = ( wins + draws + losses )
+ return total
+ 
+if __name__ == "__main__":
+ print(league_points(3, 4, 2))
